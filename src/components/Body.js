296d9b5a8c42572598476091fbf2f5f4277562ad
ankit <ankit.cni99@gmail.com>
2023-10-14 20:30:41 +0530

    before starting

--- src/components/Body.js
+++ src/components/Body.js
@@ -3,18 +3,21 @@ import data from '../dummydata'
 import Roomcards from '../components/roomcards'
 
 const Body = (props) => {
+    
     return (
-        <div className="container">
-            <div className="row">
-                {data.map(dataItem => (
-                    <div className="col-sm">
-                        <Roomcards
-                            key={dataItem.key}
-                            title={dataItem.title}
-                            content={dataItem.content}
-                        />
-                    </div>
-                ))}
+        <div className="cards-section">
+            <div className="container">
+                <div className="row">
+                    {data.map(dataItem => (
+                        <div className="col-sm">
+                            <Roomcards
+                                key={dataItem.key}
+                                title={dataItem.title}
+                                content={dataItem.content}
+                            />
+                        </div>
+                    ))}
+                </div>
             </div>
         </div>
     )
