adbb7be49bbf33cf575e27d0a556673a25831c65
ankit7458 <91431584+ankit7458@users.noreply.github.com>
2023-10-15 18:27:43 +0530

    creating structure

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,24 +1,28 @@
 import React from "react";
 import data from '../dummydata'
 import Roomcards from '../components/roomcards'
+import Footer from '../components/Footer'
 
 const Body = (props) => {
-    
+
     return (
-        <div className="cards-section">
-            <div className="container">
-                <div className="row">
-                    {data.map(dataItem => (
-                        <div className="col-sm">
-                            <Roomcards
-                                key={dataItem.key}
-                                title={dataItem.title}
-                                content={dataItem.content}
-                            />
-                        </div>
-                    ))}
+        <div className="body-c">
+            <div className="cards-section">
+                <div className="container">
+                    <div className="row">
+                        {data.map(dataItem => (
+                            <div className="col-sm">
+                                <Roomcards
+                                    key={dataItem.key}
+                                    title={dataItem.title}
+                                    content={dataItem.content}
+                                />
+                            </div>
+                        ))}
+                    </div>
                 </div>
             </div>
+            <Footer />
         </div>
     )
 }
