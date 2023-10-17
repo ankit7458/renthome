b263be4d209ca271635698e4efbc90b47fafd68d
RahulGupta <rahulrgd21@gmail.com>
2023-10-17 14:16:10 +0530

    Updated Navbar - Implemented Scrolable Navbar.Brand, fixed form search-place holder & button, added login Button

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,68 +1,61 @@
-const Navbar = () => {
-  return (
-    <div>
-      <nav className="navbar navbar-expand-lg fixed-top bg-dark navbar-c">
-        <div className="container-fluid ">
-          <div className="container">
-            <div className="row">
-              <div className="col-sm">
-                <div className="row">
-                  <div className="col-sm">
-                    <a className="navbar-brand text-white" href="#">
-                      RentHome.com
-                    </a>
-                    <button
-                      className="navbar-toggler"
-                      type="button"
-                      data-bs-toggle="collapse"
-                      data-bs-target="#navbarSupportedContent"
-                      aria-controls="navbarSupportedContent"
-                      aria-expanded="false"
-                      aria-label="Toggle navigation"
-                    >
-                      <span className="navbar-toggler-icon"></span>
-                    </button>
-                  </div>
-                  <div className="col-sm">
-                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                      <li className="nav-item">
-                        <a
-                          className="nav-link active text-white"
-                          aria-current="page"
-                          href="#"
-                        >
-                          Home
-                        </a>
-                      </li>
-                    </ul>
-                  </div>
-                </div>
-              </div>
-              <div className="col-sm">
-                {/* <div
-                  className="collapse navbar-collapse"
-                  id="navbarSupportedContent"
-                >
-                  <form className="d-flex search-bar" role="search">
-                    <input
-                      className="form-control me-2"
-                      type="search"
-                      placeholder="Search"
-                      aria-label="Search"
-                    />
-                    <button className="btn btn-outline-success" type="submit">
-                      Search
-                    </button>
-                  </form>
-                </div> */}
-              </div>
-              <div className="col-sm"></div>
-            </div>
-          </div>
-        </div>
-      </nav>
-    </div>
-  );
-};
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import InputGroup from 'react-bootstrap/InputGroup';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
-export default Navbar;
+function MyNavbar() {
+  return (
+    <Navbar fixed="top" expand="lg" className="bg-body-tertiary ">
+      <Container fluid>
+      <Navbar.Brand href="#">RentHome.com</Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          <Nav
+            className="me-auto my-2 my-lg-0"
+            style={{ maxHeight: '100px' }}
+            navbarScroll
+          >
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link>
+            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+        
+      <Form inline>
+        <Row>
+          <Col xs="auto">
+            <Form.Control
+              type="text"
+              placeholder="Search"
+              className=" mr-sm-2"
+            />
+          </Col>
+          <Col xs="auto">
+            <Button type="submit">Search</Button>
+          </Col>
+        </Row>
+      </Form>
+      &nbsp;
+      <Button variant="success">Login</Button>{' '}
+      </Container>
+      
+    </Navbar>
+  );
+}
+
+export default MyNavbar;
