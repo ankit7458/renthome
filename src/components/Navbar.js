226142dea802b4c38cbf1b9ae9c8d63ec7ca528c
RahulGupta <rahulrgd21@gmail.com>
2023-10-17 15:25:10 +0530

    Removed drop-down menu from navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,59 +1,45 @@
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
-import InputGroup from 'react-bootstrap/InputGroup';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import InputGroup from "react-bootstrap/InputGroup";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 function MyNavbar() {
   return (
-    <Navbar fixed="top" expand="lg" className="bg-body-tertiary ">
+    <Navbar fixed="top" expand="lg" className=" bg-dark navbar-c text-light">
       <Container fluid>
-      <Navbar.Brand href="#">RentHome.com</Navbar.Brand>
+        <Navbar.Brand href="#" className="text-light">
+          RentHome.com
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="navbarScroll" />
         <Navbar.Collapse id="navbarScroll">
           <Nav
-            className="me-auto my-2 my-lg-0"
-            style={{ maxHeight: '100px' }}
+            className="me-auto my-2 my-lg-0 bg-dark text-light"
+            style={{ maxHeight: "100px" }}
             navbarScroll
-          >
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
+          ></Nav>
         </Navbar.Collapse>
-        
-      <Form inline>
-        <Row>
-          <Col xs="auto">
-            <Form.Control
-              type="text"
-              placeholder="Search"
-              className=" mr-sm-2"
-            />
-          </Col>
-          <Col xs="auto">
-            <Button type="submit">Search</Button>
-          </Col>
-        </Row>
-      </Form>
-      &nbsp;
-      <Button variant="success">Login</Button>{' '}
+        <Form inline>
+          <Row>
+            <Col xs="auto">
+              <Form.Control
+                type="text"
+                placeholder="Search"
+                className=" mr-sm-2"
+              />
+            </Col>
+            <Col xs="auto">
+              <Button type="submit">Search</Button>
+            </Col>
+          </Row>
+        </Form>
+        &nbsp;
+        <Button variant="success">Login</Button>{" "}
       </Container>
-      
     </Navbar>
   );
 }
