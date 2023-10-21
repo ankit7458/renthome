2d2d343dce32116a6a30b40cada39131a8ced324
Saurabh01patil <saurabhpatilcs01@gmail.com>
2023-10-22 00:58:05 +0530

    Add about us page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './components/Navbar';
 import Carousel from './components/carousel';
 import Body from './components/Body';
+import Footer from './components/Footer';
 import Aboutus from './components/Aboutus'; // Import the AboutUs component
 
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // Update the import
@@ -18,6 +19,7 @@ function App() {
           <Route path="/Aboutus" element={<Aboutus />} /> {/* Render the Aboutus component for the "/aboutus" route */}
           <Route path="/" element={<Body />} />
         </Routes>
+        <Footer/>
       </div>
     </Router>
   );
