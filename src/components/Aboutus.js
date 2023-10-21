d5a0927f770416c5de0eb80863720e9e0e17701e
Saurabh01patil <saurabhpatilcs01@gmail.com>
2023-10-21 20:08:24 +0530

    working on about us page

---
+++ src/components/Aboutus.js
@@ -0,0 +1,13 @@
+// src/components/Aboutus.js
+import React from "react";
+
+function Aboutus() {
+  return (
+    <div>
+      <h2>About Us</h2>
+      <p>This is the about us page content.</p>
+    </div>
+  );
+}
+
+export default Aboutus;
