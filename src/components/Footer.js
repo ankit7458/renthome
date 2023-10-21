1f005599345fb5b0a84ff4a706df909633cbaa75
saurabh patil <saurabhpatilcs01@gmail.com>
2023-10-21 14:55:53 +0530

    checked in footer css

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,69 +1,65 @@
 import React from "react";
-
+import 'font-awesome/css/font-awesome.min.css';
 
 const Footer = (props) => {
     return (
-        // <div className="footer">
-        //     <h1>Footer will be created here</h1>
-        // </div>
-        // <!-- Remove the container if you want to extend the Footer to full width. -->
         <div className="footer">
-            <footer className="text-center text-lg-start text-white" >
+            <footer className="text-center text-lg-start text-white">
                 <div className="container p-4 pb-0">
-                    <section className="">
+                    <section>
                         <div className="row">
-                            <div className="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
-                                <h6 className="text-uppercase mb-4 font-weight-bold">
-                                    Company name
-                                </h6>
+                            <hr className="w-100 clearfix d-md-none" />
+                            <div className="contact col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
+                                <h6 className="text-uppercase mb-4 font-weight-bold">Contact</h6>
                                 <p>
-                                    Here you can use rows and columns to organize your footer
-                                    content. Lorem ipsum dolor sit amet, consectetur adipisicing
-                                    elit.
+                                    <a className="text-white">Email: info@renthome.com</a>
+                                </p>
+                                <p>
+                                    <a className="text-white">Phone: +1-123-456-7890</a>
+                                </p>
+                                <p>
+                                    <a className="text-white">123 Main St, City, Country</a>
+                                </p>
+                                <p>
+                                    <a className="text-white">Skype: renthome-skype</a>
                                 </p>
                             </div>
                             <hr className="w-100 clearfix d-md-none" />
-                            <div className="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
-                                <h6 className="text-uppercase mb-4 font-weight-bold">Products</h6>
-                                <p>
-                                    <a className="text-white">MDBootstrap</a>
-                                </p>
-                                <p>
-                                    <a className="text-white">MDWordPress</a>
-                                </p>
-                                <p>
-                                    <a className="text-white">BrandFlow</a>
-                                </p>
-                                <p>
-                                    <a className="text-white">Bootstrap Angular</a>
-                                </p>
-                            </div>
-                            <hr className="w-100 clearfix d-md-none" />
-                            <div className="col-md-3 col-lg-2 col-xl-2 mx-auto mt-3">
+                            <div className="link col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
                                 <h6 className="text-uppercase mb-4 font-weight-bold">
-                                    Useful links
+                                    Quick Links
                                 </h6>
-                                <p>
-                                    <a className="text-white">Your Account</a>
-                                </p>
-                                <p>
-                                    <a className="text-white">Become an Affiliate</a>
-                                </p>
-                                <p>
-                                    <a className="text-white">Shipping Rates</a>
-                                </p>
-                                <p>
-                                    <a className="text-white">Help</a>
-                                </p>
+                                <ul>
+                                    <li><a href="#">Home</a></li>
+                                    <li><a href="#">Rooms</a></li>
+                                    <li><a href="#">About Us</a></li>
+                                    <li><a href="#">Terms and Conditions</a></li>
+                                    <li><a href="#">Privacy Policy</a></li>
+                                </ul>
                             </div>
                             <hr className="w-100 clearfix d-md-none" />
                             <div className="col-md-4 col-lg-3 col-xl-3 mx-auto mt-3">
-                                <h6 className="text-uppercase mb-4 font-weight-bold">Contact</h6>
-                                <p><i class="fa-solid fa-house"></i> New York, NY 10012, US</p>
-                                <p><i class="fas fa-envelope mr-3"></i> info@gmail.com</p>
-                                <p><i class="fas fa-phone mr-3"></i> + 01 234 567 88</p>
-                                <p><i class="fas fa-print mr-3"></i> + 01 234 567 89</p>
+                                <h6 className="text-uppercase mb-4 font-weight-bold">Follow Us</h6>
+                                <ul className="social-links">
+                                    <li>
+                                        <a href="https://www.facebook.com/" className="social-link">
+                                            <i className="fab fa-facebook"></i> Facebook
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="https://www.twitter.com/" className="social-link">
+                                            <i className="fab fa-twitter"></i> Twitter
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="https://www.instagram.com/" className="social-link">
+                                            <i className="fab fa-instagram"></i> Instagram
+                                        </a>
+                                    </li>
+                                </ul>
                             </div>
+
+
                         </div>
                     </section>
                     <hr className="my-3" />
@@ -76,26 +72,25 @@ const Footer = (props) => {
                                 </div>
                             </div>
                             <div className="col-md-5 col-lg-4 ml-lg-0 text-center text-md-end">
-                                <a className="btn btn-outline-light btn-floating m-1" class="text-white" role="button"><i class="fab fa-facebook-f"></i></a>
-                                <a className="btn btn-outline-light btn-floating m-1" class="text-white" role="button"><i class="fab fa-twitter"></i></a>
-
-
-                                <a className="btn btn-outline-light btn-floating m-1" class="text-white" role="button"><i class="fab fa-google"></i></a>
-
-
-                                <a className="btn btn-outline-light btn-floating m-1" class="text-white" role="button"><i class="fab fa-instagram"></i></a>
+                                <a className="btn btn-outline-light btn-floating m-1" role="button">
+                                    <i className="fab fa-facebook-f"></i>
+                                </a>
+                                <a className="btn btn-outline-light btn-floating m-1" role="button">
+                                    <i className="fab fa-twitter"></i>
+                                </a>
+                                <a className="btn btn-outline-light btn-floating m-1" role="button">
+                                    <i className="fab fa-google"></i>
+                                </a>
+                                <a className="btn btn-outline-light btn-floating m-1" role="button">
+                                    <i className="fab fa-instagram"></i>
+                                </a>
                             </div>
-
                         </div>
                     </section>
                 </div>
-
             </footer>
-
         </div>
-
-
     )
 }
 
-export default Footer
+export default Footer;
