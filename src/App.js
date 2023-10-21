e7f543171bb60aa48501092c8bc1830175bf2619
Saurabh01patil <saurabhpatilcs01@gmail.com>
2023-10-22 01:40:56 +0530

    Reload about page

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './components/Navbar';
 import Carousel from './components/carousel';
 import Body from './components/Body';
-import Footer from './components/Footer';
 import Aboutus from './components/Aboutus'; // Import the AboutUs component
 
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // Update the import
@@ -19,7 +18,6 @@ function App() {
           <Route path="/Aboutus" element={<Aboutus />} /> {/* Render the Aboutus component for the "/aboutus" route */}
           <Route path="/" element={<Body />} />
         </Routes>
-        <Footer/>
       </div>
     </Router>
   );
