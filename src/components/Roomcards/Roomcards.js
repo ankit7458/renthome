4574ab4c4d5a79206d1ae7f9e372f9d3d956baed
Saurabh01patil <saurabhpatilcs01@gmail.com>
2023-10-24 15:34:03 +0530

    resolve conflict

--- src/components/Roomcards/Roomcards.js
+++ src/components/Roomcards/Roomcards.js
@@ -14,24 +14,20 @@ function Roomcards(props) {
     return (
 
         <Card className="card-c card-shadow "  >
-            <Link to="/roomcards/carddeatail"><Card.Img className="image-container" variant="top" src={props.img} /></Link>  
-            <Card.Body>
-                <Container>
-        <Card className="card-c card-shadow "  > 
-            <Link to="/roomcards/carddeatail"><Card.Img className="image-container" variant="top" src={props.img} /></Link> 
-                <Container>
+            <Link to="/roomcards/carddeatail"><Card.Img className="image-container" variant="top" src={props.img} /></Link>
+            <Container>
 
-                    <Row>
-                        <Col className="price-back">
-                            <p><PriceTag price={props.price} /></p>
-                        </Col>
-                        <Col>
-                            <div>
-                                <RatingIcon rating={3} /> {/* Display a 3-star rating */}
-                            </div>
-                        </Col>
-                    </Row>
-                </Container>
+                <Row>
+                    <Col className="price-back">
+                        <p><PriceTag price={props.price} /></p>
+                    </Col>
+                    <Col>
+                        <div>
+                            <RatingIcon rating={3} /> {/* Display a 3-star rating */}
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
             <Card.Body>
                 <Card.Text>
                     <p className="card-title">{props.title}</p>
