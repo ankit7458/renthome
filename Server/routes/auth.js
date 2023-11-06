ebd1f455092882150ba6f908d0fc5f7d6f573261
ankit7458 <ankit.cni99@gmail.com>
2023-11-06 21:32:23 +0530

    added for creating user

--- Server/routes/auth.js
+++ Server/routes/auth.js
@@ -1,13 +1,39 @@
 const express = require('express')
 const router = express.Router();
+// const bcrypt = require('bcryptjs');
+const { validationResult , body } = require('express-validator')
 const User = require('../models/User')
 
 
-router.get('/login',(req , res) => {
+// creating user Routes
+router.post('/createuser', [
+    body('email', 'Enter valid email').isEmail(),
+    body('password', 'Password Should meet the length').isLength({ min: 5 })
+], async (req, res) => {
 
-    const user = User(req.body);
-    user.save();
-    res.send(req.body)
+    // const errors = validationResult(req);
+    // if (!errors.isEmpty()) {
+    //     return res.status(400).json({ errors: errors.array() });
+    // }
+    try {
+        let user = await User.findOne({ email: req.body.email })
+
+        if (user) {
+            return res.status(400).json({ error: "Sorry user with this email is already exists!" })
+        }
+        //create User
+        user = await User.create({
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password
+        })
+        res.send(req.body)
+    } catch (error) {
+        console.log(error)
+        res.status(500).send('Internal Server Error')
+    }
+
+
 });
 
 
